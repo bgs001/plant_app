a227cc221787ca15da4918f2a97f77d243ae37f8
bgs001 <bobrecov.g.s@edu.narfu.ru>
2022-04-20 05:05:26 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pony_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pony_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/plant_detail_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'plant_list_page.dart';
+
+class PlantDetailPage extends StatelessWidget {
+  final int plantId;
+
+  PlantDetailPage(this.plantId);
+
+  @override
+  Widget build(BuildContext context) {
+    final plant = plants[plantId];
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Plant Detail Page"),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15)
+                    ),
+                    color: Colors.green,
+                  ),
+                  child: Text(
+                    plant.name,
+                    style: Theme.of(context).textTheme.headline4!.copyWith(color: Colors.white),
+                  )
+              ),
+              Image.network(plant.img, width: 200, height:200),
+              Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                      plant.desc,
+                      style: Theme.of(context).textTheme.bodyText1
+                  )
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
